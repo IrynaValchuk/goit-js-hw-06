98f62596ea818093c5805602e013d8f63b85a84f
Iryna <irula17ooo@gmail.com>
2023-02-04 18:16:20 +0200

    add task-1

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,6 @@ const categoriesListItem = categoriesList.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesListItem.length}`);
 
 categoriesListItem.forEach(category => {
-    console.log(`Category: ${category.firstElementChild.textContent}`);
-    console.log(`Elements: ${category.lastElementChild.children.length}`);
+  console.log(`Category: ${category.firstElementChild.textContent}`);
+  console.log(`Elements: ${category.lastElementChild.children.length}`);
 });
