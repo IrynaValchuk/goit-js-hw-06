8974415918d4d9b53fe8961cd68a9e395853b004
Iryna <irula17ooo@gmail.com>
2023-02-04 18:48:47 +0200

    add task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientsListItems = ingredients.map(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+
+  return listItem;
+});
+
+ingredientsList.append(...ingredientsListItems);
