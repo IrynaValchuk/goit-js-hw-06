695956c917c4446878eb97bed7edafd447941c2a
Iryna <irula17ooo@gmail.com>
2023-02-06 01:43:22 +0200

    update task-9

--- js/task-09.js
+++ js/task-09.js
@@ -9,5 +9,7 @@ const onBtnChangeColorClick = () => {
 buttonChangeColor.addEventListener('click', onBtnChangeColorClick);
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
