5f1e5844dbddbf294e86531b12d2af747fb4b526
Iryna <irula17ooo@gmail.com>
2023-02-06 01:31:21 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const buttonChangeColor = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+const onBtnChangeColorClick = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = document.body.style.backgroundColor;
+};
+
+buttonChangeColor.addEventListener('click', onBtnChangeColorClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
