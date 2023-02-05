5c5ff833a3c3a0bea4fad3144def1202ea551443
Iryna <irula17ooo@gmail.com>
2023-02-06 01:12:51 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formLogin = document.querySelector('.login-form');
 
+const onEnterDataForm = event => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  if (email.value === '' || password.value === '') {
+    alert('Please make sure all fields are filled ');
+  }
+  console.log(formData);
+  formLogin.reset();
+};
+
+formLogin.addEventListener('submit', onEnterDataForm);
